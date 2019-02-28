851153aeb0ea44e7a2625ab855e21cbea88a214e
Tom Jordan <mail@tom-jordan.co.uk>
2019-02-27 16:18:44 -0800

    Deleted unused css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { Provider } from "mobx-react";
 import GithubStore from './stores/GithubStore';
